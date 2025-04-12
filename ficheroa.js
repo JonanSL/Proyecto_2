730a7c47eba9d24581381b0e0f7784a371b4cdf6
Jon Ander SL <jon_ander92@hotmail.com>
2025-04-12 11:27:34 +0200

    Sincronizado con Rama_1

--- ficheroa.js
+++ ficheroa.js
@@ -1,3 +1,3 @@
-console.log("*****Ahora hemos cambiado*****")
+console.log("*****-----*****")
 console.log("Soy un fichero que se llama A")
-console.log("*****Aqui terminamos el cambio*****")
+console.log("*****-----*****")
