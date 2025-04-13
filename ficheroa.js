cce82363d238e7f8e6e8672eb36fe251b36e8a0e
Jon Ander SL <jon_ander92@hotmail.com>
2025-04-13 08:08:09 +0200

    Mod

--- ficheroa.js
+++ ficheroa.js
@@ -1,3 +1,5 @@
-console.log("***-----***")
-console.log("Soy el fichero A y me an actualizado en GitHub")
-console.log("***-----***")
+console.log("***-----***");
+console.log("Soy el fichero A y me an actualizado en GitHub");
+console.log("***-----***");
+console.log("+++++++");
+console.log("Otro parchial");
