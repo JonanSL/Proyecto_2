d18b7da47d85f256bf0afce66be55f68c5bae5e0
Jon Ander SL <jon_ander92@hotmail.com>
2025-04-13 07:56:36 +0200

    Otra mod

--- ficheroa.js
+++ ficheroa.js
@@ -1,8 +1,9 @@
 console.log("***-----***");
-console.log("Soy el fichero A y me an actualizado en GitHub");
-console.log("***-----***");
-console.log("Veamos como se comporta el STASH");
-console.log("----------");
-console.log("Practicando");
 console.log("***-----***");
 console.log("***-----***");
+console.log("***-----***");
+console.log("***-----***");
+console.log("***-----***");
+console.log("***-----***");
+console.log("***-----***");
+
