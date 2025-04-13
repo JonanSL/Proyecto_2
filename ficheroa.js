ffa94cf889a125901bd7257bc9c4284661ce2f2c
Jon Ander SL <jon_ander92@hotmail.com>
2025-04-13 07:32:08 +0200

    Stasheando

--- ficheroa.js
+++ ficheroa.js
@@ -2,3 +2,5 @@ console.log("*****-----*****");
 console.log("Soy un fichero que se llama A");
 console.log("*****-----*****");
 console.log("Voy a hacer un STASH");
+console.log("Hemos añadido 2 ficheros a .gitignore");
+console.log("Veamos como se comporta el STASH")
--- ficheroe.js
+++ ficheroe.js
@@ -1,1 +1,2 @@
 console.log("Creando nuevo fichero para Stash");
+console.log("Vamos a comentar el Stash");
