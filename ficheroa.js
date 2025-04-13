ef9ca3010d588ca42049ed90d74429a5f3e26936
Jon Ander SL <jon_ander92@hotmail.com>
2025-04-13 06:53:11 +0200

    Mod. fichero A y B para hacer STASH

--- ficheroa.js
+++ ficheroa.js
@@ -1,3 +1,4 @@
-console.log("*****-----*****")
-console.log("Soy un fichero que se llama A")
-console.log("*****-----*****")
+console.log("*****-----*****");
+console.log("Soy un fichero que se llama A");
+console.log("*****-----*****");
+console.log("Voy a hacer un STASH");
--- ficherob.js
+++ ficherob.js
@@ -1,3 +1,4 @@
-console.log("++++++++++")
-console.log("Soy un fichero II")
-console.log("++++++++++")
+console.log("++++++++++");
+console.log("Soy un fichero II");
+console.log("++++++++++");
+console.log("Estamos practicando un STASH");
