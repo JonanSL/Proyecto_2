ed166d4a67410bb15658b8933eea1fba0e3bc655
Jon Ander SL <jon_ander92@hotmail.com>
2025-04-13 07:38:43 +0200

    Merge branch 'Rama_1'

--- ficheroa.js
+++ ficheroa.js
@@ -1,3 +1,8 @@
-console.log("***-----***")
-console.log("Soy el fichero A y me an actualizado en GitHub")
-console.log("***-----***")
+console.log("***-----***");
+console.log("Soy el fichero A y me an actualizado en GitHub");
+console.log("***-----***");
+console.log("Veamos como se comporta el STASH");
+console.log("----------");
+console.log("Practicando");
+console.log("***-----***");
+console.log("***-----***");
