ce907486542c14dd1309b32d9b7614d052d8a6a7
Jon Ander SL <jon_ander92@hotmail.com>
2025-04-13 08:23:51 +0200

    otro Commit

--- ficheroa.js
+++ ficheroa.js
@@ -2,10 +2,6 @@ console.log("***-----***");
 console.log("Soy el fichero A y me an actualizado en GitHub");
 console.log("***-----***");
 console.log("Veamos como se comporta el STASH");
-console.log("----------");
-console.log("Practicando");
-console.log("Seguimos con STASH diferentes");
-console.log("Seguimos con STASH diferentes");
-console.log("Seguimos con STASH diferentes");
-console.log("Seguimos con STASH diferentes");
+
+
 
